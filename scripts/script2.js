5258b151955d258be60e6bf993464b3a31767b3a
Stanislav Vasilevich <stanislavasilevich@yandex.ru>
2021-10-27 11:36:48 +0300

    feat: сделал структуру файлов и папок, добавил файл README.md, также добавил в него описание, добавил задание для 3 урока

---
+++ scripts/script2.js
@@ -0,0 +1,137 @@
+// Работа с переменными
+/* 1
+const num = 3;
+alert(num);
+*/
+
+/* 2
+const a = 10;
+const b = 2;
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+*/
+
+/* 3
+const c = 15;
+const d = 2;
+const result = c + d;
+
+console.log(result);
+*/
+
+/* 4
+const a = 10;
+const b = 2;
+const c = 5;
+
+console.log(a + b + c);
+*/
+
+/* 5
+const a = 17;
+const b = 10;
+const c = a - b;
+const d = 7;
+const result = c + d;
+
+console.log(result);
+*/
+
+// Работа со строками
+/* 1
+const str = 'Привет, Мир!'
+
+console.log(str);
+*/
+
+/* 2
+const str1 = 'Привет, ';
+const str2 = 'Мир!';
+
+console.log(str1 + str2);
+*/
+
+/* 3
+const name = 'Станислав';
+
+console.log('Привет, ' + name + '!');
+*/
+
+/* 4
+const age = 33;
+
+console.log('Мне ' + age + ' года!');
+*/
+
+// Функция prompt
+/* 1
+const name = prompt('Назовите Ваше имя:');
+alert('Ваше имя ' + name);
+*/
+
+/* 2
+const num = prompt('Введите число: ');
+alert(num * num);
+*/
+
+// Обращение к символам строки
+/* 1
+const str = 'abcde';
+
+console.log(str[0], str[2], str[4]);
+*/
+
+/* 2
+const num = '12345';
+
+console.log(num[0] * num[1] * num[2] * num[3] * num[4]);
+*/
+
+// Практика
+/* 1
+const seconds = 1;
+const hour = 60;
+const day = 24;
+const month = 30;
+const secondsHour = seconds * hour;
+const hoursDay = secondsHour * day;
+const daysMonth = hoursDay * month;
+
+console.log(hoursDay, daysMonth);
+*/
+
+/* 2
+const hour = 10;
+const minute = 22;
+const seconds = 44;
+
+console.log(hour + ':' + minute + ':' + seconds);
+*/
+
+/* 3
+const num = 10;
+const numSquare = num * num;
+
+console.log(numSquare);
+*/
+
+// Работа с присваиванием и декрементами
+/* 1
+var num = 47; 
+num += 7;
+num -=18;
+num *= 10;
+num /= 15;
+alert(num);
+*/
+
+/*
+var num = 10; 
+num++
+num++
+num --
+alert(num);
+*/
