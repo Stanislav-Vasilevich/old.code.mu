8bf35deff26803d719d4351dc9ee6754fb2b2899
Stanislav Vasilevich <stanislavasilevich@yandex.ru>
2021-10-28 11:58:16 +0300

    feat: начал выполнение 5 урока

---
+++ scripts/script5.js
@@ -0,0 +1,28 @@
+// Циклы while и for
+/* 1
+for (let i = 1; i <= 100; i++) {
+  console.log(i);
+}
+*/
+
+/* 2
+for (let i = 11; i <= 33; i++) {
+  console.log(i);
+}
+*/
+
+/*
+for (let i = 0; i <= 100; i += 2) {
+  console.log(i);
+}
+*/
+
+/*
+let result = 0;
+
+for (let i = 1; i <= 100; i++) {
+  result += i
+}
+
+console.log('result: ', result);
+*/
