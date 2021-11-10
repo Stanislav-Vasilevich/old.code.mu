fc113249c6706477b4418d4b144e01a5e88524f2
Stanislav Vasilevich <stanislavasilevich@yandex.ru>
2021-11-10 11:54:03 +0300

    feat: остановился на выполнении 7 задания

---
+++ scripts/script7.js
@@ -0,0 +1,59 @@
+// Работа с регистром символов
+/* 1
+const str = 'js';
+
+console.log(str.toUpperCase());
+*/
+
+/* 2
+const str = 'JS';
+
+console.log(str.toLowerCase());
+*/
+
+// Работа с length, substr, substring, slice. Работа с indexOf
+/* 1
+const str = 'я учу javascript!';
+
+console.log(str.length);
+*/
+
+/* 2
+const str = 'я учу javascript!';
+
+console.log(str.substr(2, 3));
+console.log(str.substr(6, 10));
+console.log(str.substring(2, 6));
+console.log(str.substring(6, 16));
+console.log(str.slice(2, 6));
+console.log(str.slice(6, 16));
+*/
+
+/* 3
+const str = 'я учу javascript!';
+
+console.log(str.indexOf('учу'));
+*/
+
+/* 4*/
+
+
+// Работа с replace
+/* 1*/
+
+// Работа с split
+/* 1*/
+
+/* 2*/
+
+/* 3*/
+
+// Работа с join
+/* 1*/
+
+// Задачи
+/* 1*/
+
+/* 2*/
+
+/* 3*/
