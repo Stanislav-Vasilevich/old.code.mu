def0ee030954bc2585f1a5378239c64e23370e47
Stanislav Vasilevich <stanislavasilevich@yandex.ru>
2021-11-11 12:00:05 +0300

    feat: выполнил задания четыре задания 18 урока

---
+++ scripts/script18.js
@@ -0,0 +1,44 @@
+// Отработка циклов
+/* 1
+for(let i = 0; i <= 100; i++) {
+  console.log(i);
+}
+*/
+
+/* 2
+for(let i = 100; i >= 1; i--) {
+  console.log(i);
+}
+*/
+
+/* 3
+for(let i = 2; i <= 100; i += 2) {
+  console.log(i);
+}
+*/
+
+/* 4
+const arr = [];
+
+for(let i = 0; i < 10; i++) {
+  arr.push('X');
+}
+
+console.log(arr)
+*/
+
+/* 5*/
+
+/* 6*/
+
+/* 7*/
+
+/* 8*/
+
+/* 9*/
+
+/* 10*/
+
+/* 11*/
+
+/* 12*/
